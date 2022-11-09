3e7b0acdf634a93559429e9a4cd5f1f8fc275a2f
Javier  Carreño <35305955+car251290@users.noreply.github.com>
2022-11-09 10:05:15 -0800

    Add files via upload

--- src/components/Wrapper/WrapperStyles.js
+++ src/components/Wrapper/WrapperStyles.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import { starsBackground } from './starsBackground';
+import { starsBackground} from './starsBackground';
 
 export const Container = styled.div`
     position: relative;
